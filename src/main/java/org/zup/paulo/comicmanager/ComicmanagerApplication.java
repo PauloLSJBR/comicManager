f32d2bb46a287cf6de8ce62fcb3120156b0894fd
Paulo Lopes <paulolsjbr@gmail.com>
2021-12-12 21:01:10 -0300

    Primeiro commit

---
+++ src/main/java/org/zup/paulo/comicmanager/ComicmanagerApplication.java
@@ -0,0 +1,13 @@
+package org.zup.paulo.comicmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/zup/paulo/comicmanager/ComicmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zup.paulo.comicmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
